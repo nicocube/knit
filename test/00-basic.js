6b252ecfd8f7669d28745def06dd03b92ab25529
Nicolas Lochet <nicolas.lochet@gmail.com>
2012-03-30 15:21:05 +0200

    some explanation into the README, improvement and stuff

--- test/00-basic.js
+++ test/00-basic.js
@@ -54,60 +54,81 @@
 	});
 	
 	exports["should retrieve native node module and function binding defined dependencies"] = independent_require(function (knit,test) {
-		var _z = {}, _count = 0, _w, _x, _y, _a, _e;
+		var _z = {}, _count = 0, _w, _x, _y, _a, _f;
 		var _b = function b() {};
 		var _c = function c() {return _z;};
 		var _d = function d() {this.c = _count++;/*console.log('c',this.c);*/};
+		var _e = function e() {this.c = _count++;/*console.log('c',this.c);*/};
 		knit.config(function (bind) {
 			bind('w').to({});
 			bind('x').to({}).is('singleton');
 			bind('y').to({}).is('clone');
 			bind('z').to(_c).is('builder');
 			bind('a').to(_d).is('constructor');
-			bind(_b);
+			bind(_b).is('singleton');
 			bind(_c).is('builder');
-			bind(_d).is('constructor');
-			
-		},true).inject(function(w,x) {
+			bind(_d);
+			bind(_e).is('constructor');			
+		})
+		.inject(function(w,x) {
 			// init singletons intances
 			_w = w;
 			_x = x;
-		},true).inject(function(w,x) {
+		},true)
+		.inject(function(w,x) {
 			// validate singleton instances
 			test.strictEqual(w, _w);
 			test.strictEqual(x, _x);
-		},true).inject(function(z) {
+		},true)
+		.inject(function(z) {
 			// validate builder cases
 			test.strictEqual(z, _z);
 		},true).inject(function(y) {
 			// init prototype instances
 			_y = y;
-		},true).inject(function(y) {
+		},true)
+		.inject(function(y) {
 			// validate prototype instances
 			test.notStrictEqual(y, _y);
 			test.deepEqual(y, _y);
-		},true).inject(function(a) {
+		},true)
+		.inject(function(a) {
 			// init constructor instances
 			_a = a;
-		},true).inject(function(a) {
+		},true)
+		.inject(function(a) {
 			// validate constructor instances
 			test.notStrictEqual(a, _a);
 			test.equal(_a.c, 0);
 			test.equal(a.c, 1);
-		},true).inject(function(b) {
+		},true)
+		.inject(function(b) {
 			// validate function singleton cases
 			test.strictEqual(b, _b);
-		},true).inject(function(c) {
+		},true)
+		.inject(function(c) {
 			// validate function builder cases
 			test.strictEqual(c, _z);
-		},true).inject(function(d) {
+		},true)
+		.inject(function(d) {
 			// init function constructor cases
-			_e = d;
-		},true).inject(function(d) {
+			_f = d;
+		},true)
+		.inject(function(d) {
 			// validate function builder cases
 			test.notStrictEqual(d, _e);
-			test.equal(_e.c, 2);
+			test.equal(_f.c, 2);
 			test.equal(d.c, 3);
+		},true)
+		.inject(function(e) {
+			// init function constructor cases
+			_f = e;
+		},true)
+		.inject(function(e) {
+			// validate function builder cases
+			test.notStrictEqual(e, _e);
+			test.equal(_f.c, 4);
+			test.equal(e.c, 5);
 		},true);
 		test.done();
 	});
